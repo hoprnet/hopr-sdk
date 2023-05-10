e6efbb0371acb8978afb3541b1219686b9a74ee1
Diego Alzate <diego35.da@gmail.com>
2023-05-10 11:22:53 -0500

    remove buffer dependency

--- src/utils/headers.ts
+++ src/utils/headers.ts
@@ -1,13 +1,9 @@
 import { Headers } from 'cross-fetch';
-import { Buffer } from 'buffer';
 
 export const getHeaders = (apiKey: string): Headers => {
   const headers = new Headers();
   headers.set('Content-Type', 'application/json');
   headers.set('Accept-Content', 'application/json');
-  headers.set(
-    'Authorization',
-    `Basic ${Buffer.from(apiKey, 'utf-8').toString('base64')}`
-  );
+  headers.set('x-auth-token', `${apiKey}`);
   return headers;
 };
