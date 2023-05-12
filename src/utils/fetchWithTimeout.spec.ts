ffc86609b495a260efd2c9c86f5e12509b8b008b
Diego Alzate <diego35.da@gmail.com>
2023-05-12 17:01:47 -0500

    add fetch with timeout function

---
+++ src/utils/fetchWithTimeout.spec.ts
@@ -0,0 +1,38 @@
+import { fetchWithTimeout } from './fetchWithTimeout'; // Import fetchWithTimeout from your module
+import nock from 'nock';
+
+describe('fetchWithTimeout', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+    nock.cleanAll();
+  });
+
+  it('should abort the request if it takes longer than the specified timeout', async () => {
+    const url = new URL('https://example.com');
+    const options = { method: 'GET' };
+    const ms = 1000;
+
+    nock(url.origin)
+      .get(url.pathname)
+      .delay(ms + 100) // Delay the response to trigger a timeout
+      .reply(200, 'Mock data');
+
+    await expect(fetchWithTimeout(url, options, ms)).rejects.toThrowError(
+      'TIMEOUT'
+    );
+  });
+
+  it('should return the response when the request completes within the timeout', async () => {
+    const mockResponse = { status: 200, data: 'Mock data' };
+
+    const url = new URL('https://example.com');
+    const options = { method: 'POST' };
+    const ms = 1000;
+
+    nock(url.origin).post(url.pathname).reply(200, mockResponse);
+
+    const response = await fetchWithTimeout(url, options, ms);
+    const jsonResponse = await response.json();
+    expect(jsonResponse).toEqual(mockResponse);
+  });
+});
---
+++ src/utils/fetchWithTimeout.ts
@@ -0,0 +1,21 @@
+import fetch from 'cross-fetch';
+import { APIError } from './error';
+
+export const fetchWithTimeout = (
+  url: URL | RequestInfo,
+  options: RequestInit | undefined,
+  ms: number
+) => {
+  const controller = new AbortController();
+  const promise = fetch(url, { ...options, signal: controller.signal }).catch(
+    () => {
+      throw new APIError({
+        error: 'TIMEOUT',
+        status: '504'
+      });
+    }
+  );
+  // abort promise if it cas not been completed after ms
+  const timeout = setTimeout(() => controller.abort(), ms);
+  return promise.finally(() => clearTimeout(timeout));
+};
