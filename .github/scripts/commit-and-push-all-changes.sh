8b8c4139b5c3104bfb62a9a12a8baccf7f1e9409
Luis Martínez <luis.martinez@hoprnet.org>
2023-06-02 08:15:30 -0500

    Make script push to 'docs'

--- .github/scripts/commit-and-push-all-changes.sh
+++ .github/scripts/commit-and-push-all-changes.sh
@@ -22,4 +22,4 @@ fi
 # must get the latest version of the branch from origin before pushing
 git pull origin "${HOPR_GITHUB_REF}" --rebase --strategy-option recursive -X ours # NB! when pull rebasing, ours is the incoming change (see https://stackoverflow.com/a/3443225)
 
-git push origin "${HOPR_GITHUB_REF}"
+git push origin docs
