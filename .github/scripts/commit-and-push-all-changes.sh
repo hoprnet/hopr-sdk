5d32c465a89affe557922d875145e4b995f06c0e
Luis Martínez <luis.martinez@hoprnet.org>
2023-06-02 14:20:09 -0500

    Fix to where we push the docs

--- .github/scripts/commit-and-push-all-changes.sh
+++ .github/scripts/commit-and-push-all-changes.sh
@@ -22,4 +22,4 @@ fi
 # must get the latest version of the branch from origin before pushing
 git pull origin "${HOPR_GITHUB_REF}" --rebase --strategy-option recursive -X ours # NB! when pull rebasing, ours is the incoming change (see https://stackoverflow.com/a/3443225)
 
-git push origin docs
+git push origin HEAD:"refs/heads/docs" # Push changes to the 'docs' branch
