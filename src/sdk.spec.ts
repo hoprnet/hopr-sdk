2c7d5d022776951671f49848e858fb79d5810a69
Diego Alzate <diego35.da@gmail.com>
2023-06-05 09:57:34 -0500

    fix tests

--- src/sdk.spec.ts
+++ src/sdk.spec.ts
@@ -1,15 +1,15 @@
 import nock from 'nock';
-import { HoprSdk } from './sdk';
+import { HoprSDK } from './sdk';
 // Set up global constants for URL and API key
-const API_URL = 'http://localhost:3001';
-const API_KEY = 'S3CR3T-T0K3N';
+const API_ENDPOINT = 'http://localhost:3001';
+const API_TOKEN = 'S3CR3T-T0K3N';
 
 describe('test HoprdSdk class', function () {
-  let sdk: HoprSdk;
+  let sdk: HoprSDK;
   beforeEach(function () {
-    sdk = new HoprSdk({
-      apiKey: API_KEY,
-      url: API_URL
+    sdk = new HoprSDK({
+      apiToken: API_TOKEN,
+      apiEndpoint: API_ENDPOINT
     });
   });
 
@@ -17,7 +17,7 @@ describe('test HoprdSdk class', function () {
     it('should not send if node has no outgoing channel', async function () {
       const sendMessageSpy = jest.spyOn(sdk.api.messages, 'sendMessage');
       // mock hoprd node channels
-      nock(API_URL)
+      nock(API_ENDPOINT)
         .get('/api/v2/channels')
         .reply(200, {
           incoming: [],
@@ -42,7 +42,7 @@ describe('test HoprdSdk class', function () {
     it('should send message', async function () {
       const sendMessageSpy = jest.spyOn(sdk.api.messages, 'sendMessage');
       // mock hoprd node channels
-      nock(API_URL)
+      nock(API_ENDPOINT)
         .get('/api/v2/channels')
         .reply(200, {
           incoming: [],
@@ -59,7 +59,7 @@ describe('test HoprdSdk class', function () {
         });
 
       // mock hoprd node messages
-      nock(API_URL).post('/api/v2/messages').reply(202, 'challenge-token');
+      nock(API_ENDPOINT).post('/api/v2/messages').reply(202, 'challenge-token');
 
       const challenge = await sdk.safeSendMessage({
         body: 'body',
@@ -77,7 +77,7 @@ describe('test HoprdSdk class', function () {
       const closeChannelSpy = jest.spyOn(sdk.api.channels, 'closeChannel');
       const openOutgoingChannels = ['1', '2', '3'];
       // mock hoprd node get channels
-      nock(API_URL)
+      nock(API_ENDPOINT)
         .get('/api/v2/channels')
         .reply(200, {
           incoming: [],
@@ -92,7 +92,7 @@ describe('test HoprdSdk class', function () {
         });
 
       // mock hoprd node tickets statistics
-      nock(API_URL).get(`/api/v2/tickets/statistics`).reply(200, {
+      nock(API_ENDPOINT).get(`/api/v2/tickets/statistics`).reply(200, {
         pending: 0,
         unredeemed: 0,
         unredeemedValue: 'string',
@@ -107,11 +107,13 @@ describe('test HoprdSdk class', function () {
 
       // mock hoprd node close channels
       openOutgoingChannels.map((id) => {
-        nock(API_URL).delete(`/api/v2/channels/${id}/outgoing`).reply(200, {
-          receipt:
-            '0x37954ca4a630aa28f045df2e8e604cae22071046042e557355acf00f4ef20d2e',
-          channelStatus: 'Closed'
-        });
+        nock(API_ENDPOINT)
+          .delete(`/api/v2/channels/${id}/outgoing`)
+          .reply(200, {
+            receipt:
+              '0x37954ca4a630aa28f045df2e8e604cae22071046042e557355acf00f4ef20d2e',
+            channelStatus: 'Closed'
+          });
       });
 
       const res = await sdk.closeEverything();
@@ -126,7 +128,7 @@ describe('test HoprdSdk class', function () {
       const closeChannelSpy = jest.spyOn(sdk.api.channels, 'closeChannel');
       const openOutgoingChannels = ['1', '2', '3'];
       // mock hoprd node get channels
-      nock(API_URL)
+      nock(API_ENDPOINT)
         .get('/api/v2/channels')
         .reply(200, {
           incoming: [],
@@ -141,7 +143,7 @@ describe('test HoprdSdk class', function () {
         });
 
       // mock hoprd node tickets statistics
-      nock(API_URL).get(`/api/v2/tickets/statistics`).reply(200, {
+      nock(API_ENDPOINT).get(`/api/v2/tickets/statistics`).reply(200, {
         pending: 0,
         unredeemed: 0,
         unredeemedValue: 'string',
@@ -156,11 +158,13 @@ describe('test HoprdSdk class', function () {
 
       // mock hoprd node close channels
       openOutgoingChannels.map((id) => {
-        nock(API_URL).delete(`/api/v2/channels/${id}/outgoing`).reply(200, {
-          receipt:
-            '0x37954ca4a630aa28f045df2e8e604cae22071046042e557355acf00f4ef20d2e',
-          channelStatus: 'Closed'
-        });
+        nock(API_ENDPOINT)
+          .delete(`/api/v2/channels/${id}/outgoing`)
+          .reply(200, {
+            receipt:
+              '0x37954ca4a630aa28f045df2e8e604cae22071046042e557355acf00f4ef20d2e',
+            channelStatus: 'Closed'
+          });
       });
 
       const res = await sdk.closeEverything();
@@ -171,13 +175,13 @@ describe('test HoprdSdk class', function () {
     it('redeems pending tickets', async function () {
       const redeemChannelSpy = jest.spyOn(sdk.api.tickets, 'redeemTickets');
       // mock hoprd node get channels
-      nock(API_URL).get('/api/v2/channels').reply(200, {
+      nock(API_ENDPOINT).get('/api/v2/channels').reply(200, {
         incoming: [],
         outgoing: []
       });
 
       // mock hoprd node tickets statistics
-      nock(API_URL).get(`/api/v2/tickets/statistics`).reply(200, {
+      nock(API_ENDPOINT).get(`/api/v2/tickets/statistics`).reply(200, {
         pending: 1,
         unredeemed: 0,
         unredeemedValue: 'string',
@@ -191,7 +195,7 @@ describe('test HoprdSdk class', function () {
       });
 
       // mock hoprd redeem tickets
-      nock(API_URL).post(`/api/v2/tickets/redeem`).reply(204);
+      nock(API_ENDPOINT).post(`/api/v2/tickets/redeem`).reply(204);
 
       const res = await sdk.closeEverything();
       expect(res.closedChannels.length).toEqual(0);
@@ -201,13 +205,13 @@ describe('test HoprdSdk class', function () {
     it('does not try to redeem tickets if no tickets are pending', async function () {
       const redeemChannelSpy = jest.spyOn(sdk.api.tickets, 'redeemTickets');
       // mock hoprd node get channels
-      nock(API_URL).get('/api/v2/channels').reply(200, {
+      nock(API_ENDPOINT).get('/api/v2/channels').reply(200, {
         incoming: [],
         outgoing: []
       });
 
       // mock hoprd node tickets statistics
-      nock(API_URL).get(`/api/v2/tickets/statistics`).reply(200, {
+      nock(API_ENDPOINT).get(`/api/v2/tickets/statistics`).reply(200, {
         pending: 0,
         unredeemed: 0,
         unredeemedValue: 'string',
@@ -221,7 +225,7 @@ describe('test HoprdSdk class', function () {
       });
 
       // mock hoprd redeem tickets
-      nock(API_URL).post(`/api/v2/tickets/redeem`).reply(204);
+      nock(API_ENDPOINT).post(`/api/v2/tickets/redeem`).reply(204);
 
       const res = await sdk.closeEverything();
       expect(res.closedChannels.length).toEqual(0);
@@ -233,7 +237,7 @@ describe('test HoprdSdk class', function () {
   describe('cashOut', function () {
     it('does not call withdraw without balance', async function () {
       // mock hoprd node get balances
-      nock(API_URL).get('/api/v2/account/balances').reply(200, {
+      nock(API_ENDPOINT).get('/api/v2/account/balances').reply(200, {
         native: '0',
         hopr: '0'
       });
@@ -249,13 +253,13 @@ describe('test HoprdSdk class', function () {
     });
     it('sends tx to recipient', async function () {
       // mock hoprd node get balances
-      nock(API_URL).get('/api/v2/account/balances').reply(200, {
+      nock(API_ENDPOINT).get('/api/v2/account/balances').reply(200, {
         native: '10',
         hopr: '10'
       });
       // mock hoprd node withdraw response
       const expectedReceipt = '0x123456789abcdef';
-      nock(API_URL)
+      nock(API_ENDPOINT)
         .post('/api/v2/account/withdraw')
         .times(2)
         .reply(200, { receipt: expectedReceipt });
@@ -277,7 +281,7 @@ describe('test HoprdSdk class', function () {
   describe('openMultipleChannels', function () {
     it('should not attempt to open channels if node does not have enough balance', async function () {
       // mock hoprd node get balances
-      nock(API_URL).get('/api/v2/account/balances').reply(200, {
+      nock(API_ENDPOINT).get('/api/v2/account/balances').reply(200, {
         native: '10',
         hopr: '0'
       });
@@ -294,7 +298,7 @@ describe('test HoprdSdk class', function () {
     it('should open channels', async function () {
       const peerIds = ['id1', 'id2'];
       // mock hoprd node get balances
-      nock(API_URL)
+      nock(API_ENDPOINT)
         .get('/api/v2/account/balances')
         .reply(200, {
           native: BigInt(0.03 * 10e18).toString(),
@@ -302,12 +306,15 @@ describe('test HoprdSdk class', function () {
         });
 
       // mock hoprd node open channel
-      nock(API_URL).post('/api/v2/channels').times(peerIds.length).reply(201, {
-        channelId:
-          '0x04e50b7ddce9770f58cebe51f33b472c92d1c40384759f5a0b1025220bf15ec5',
-        receipt:
-          '0x37954ca4a630aa28f045df2e8e604cae22071046042e557355acf00f4ef20d2e'
-      });
+      nock(API_ENDPOINT)
+        .post('/api/v2/channels')
+        .times(peerIds.length)
+        .reply(201, {
+          channelId:
+            '0x04e50b7ddce9770f58cebe51f33b472c92d1c40384759f5a0b1025220bf15ec5',
+          receipt:
+            '0x37954ca4a630aa28f045df2e8e604cae22071046042e557355acf00f4ef20d2e'
+        });
 
       const openChannelSpy = jest.spyOn(sdk.api.channels, 'openChannel');
 
