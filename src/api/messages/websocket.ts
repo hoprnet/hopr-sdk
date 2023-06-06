cd98624c963a84f9e704b18912b1e003cc1962ca
Michał Jadach <sarah.smith@email.com>
2023-06-06 12:20:39 +0200

    moved generateWsUrl to utils

--- src/api/messages/websocket.ts
+++ src/api/messages/websocket.ts
@@ -1,7 +1,6 @@
 import WebSocket from 'isomorphic-ws';
-import { BasePayloadType, GetWsUrlType } from '../../types';
-
-const WS_PATH = '/api/v2/messages/websocket/';
+import { BasePayloadType } from '../../types';
+import { generateWsUrl } from '../../utils';
 
 /**
  * Creates a WebSocket instance with the specified IP and API token.
@@ -12,25 +11,9 @@ const WS_PATH = '/api/v2/messages/websocket/';
  * @returns A WebSocket instance.
  */
 export const websocket = (payload: BasePayloadType): WebSocket => {
-  const endpointUrl = getWsUrl({
+  const endpointUrl = generateWsUrl({
     apiEndpoint: payload.apiEndpoint,
-    apiToken: payload.apiToken,
-    path: WS_PATH
+    apiToken: payload.apiToken
   });
   return new WebSocket(endpointUrl);
-};
-
-/**
- *
- * @param apiEndpoint - The API endpoint to extract the ip and port
- * @param apiToken - The API token to use for authentication.
- * @param path - The API path for websocket endpoing. By default: '/api/v2/messages/websocket/'
- * @returns A string of the complete API endpoint.
- */
-export const getWsUrl = (payload: GetWsUrlType): string => {
-  const path = payload.path ? payload.path : WS_PATH;
-  const url = new URL(path, payload.apiEndpoint);
-  url.protocol = url.protocol === 'https:' ? 'wss' : 'ws';
-  url.search = `?apiToken=${payload.apiToken}`;
-  return url.toString();
 };
