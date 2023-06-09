659e9e06a7d0a4d93b0eb7fe957d37f2087c86ca
Diego Alzate <diego35.da@gmail.com>
2023-06-09 11:10:21 -0500

    update peer info and settings

--- src/api/peerInfo/getPeerInfo.ts
+++ src/api/peerInfo/getPeerInfo.ts
@@ -3,8 +3,9 @@ import {
   GetPeerInfoPayloadType,
   GetPeerInfoResponse,
   GetPeerInfoResponseType,
-  Error
+  APIErrorResponse
 } from '../../types';
+import { ZodError } from 'zod';
 
 export const getPeerInfo = async (
   payload: GetPeerInfoPayloadType
@@ -23,16 +24,23 @@ export const getPeerInfo = async (
 
   const parsedRes = GetPeerInfoResponse.safeParse(jsonResponse);
 
+  // received expected response
   if (parsedRes.success) {
     return parsedRes.data;
-  } else if (rawResponse.status > 499) {
-    // server error that was unexpected
-    throw new APIError({
-      status: rawResponse.status.toString(),
-      error: rawResponse.statusText
-    });
-  } else {
-    // response is neither successful nor unexpected
-    throw new APIError(Error.parse(jsonResponse));
   }
+
+  // received unexpected error from server
+  if (rawResponse.status > 499) {
+    throw new Error(rawResponse.statusText);
+  }
+
+  // check if response has the structure of an expected api error
+  const isApiErrorResponse = APIErrorResponse.safeParse(jsonResponse);
+
+  if (isApiErrorResponse.success) {
+    throw new APIError(isApiErrorResponse.data);
+  }
+
+  // we could not parse the response and it is not unexpected
+  throw new ZodError(parsedRes.error.issues);
 };
--- src/api/settings/setSetting.ts
+++ src/api/settings/setSetting.ts
@@ -1,4 +1,5 @@
-import { Error, SetSettingPayloadType } from '../../types';
+import { ZodError } from 'zod';
+import { APIErrorResponse, type SetSettingPayloadType } from '../../types';
 import { APIError, fetchWithTimeout, getHeaders } from '../../utils';
 
 export const setSetting = async (
@@ -14,17 +15,24 @@ export const setSetting = async (
     payload.timeout
   );
 
+  // received expected response
   if (rawResponse.status === 204) {
     return true;
-  } else if (rawResponse.status > 499) {
-    // server error that was unexpected
-    throw new APIError({
-      status: rawResponse.status.toString(),
-      error: rawResponse.statusText
-    });
-  } else {
-    // response is neither successful nor unexpected
-    const jsonResponse = await rawResponse.json();
-    throw new APIError(Error.parse(jsonResponse));
   }
+
+  // received unexpected error from server
+  if (rawResponse.status > 499) {
+    throw new Error(rawResponse.statusText);
+  }
+
+  // check if response has the structure of an expected api error
+  const jsonResponse = await rawResponse.json();
+  const isApiErrorResponse = APIErrorResponse.safeParse(jsonResponse);
+
+  if (isApiErrorResponse.success) {
+    throw new APIError(isApiErrorResponse.data);
+  }
+
+  // we could not parse the error and it is not unexpected
+  throw new ZodError(isApiErrorResponse.error.issues);
 };
