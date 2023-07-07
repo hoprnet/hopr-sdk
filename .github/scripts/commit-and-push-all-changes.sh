423ba7d983f92ee8a5d27ef6f3248e2a028983f5
Luis Martínez <luis.martinez@hoprnet.org>
2023-07-07 07:48:00 -0500

    Fix commit message for docs generation

--- .github/scripts/commit-and-push-all-changes.sh
+++ .github/scripts/commit-and-push-all-changes.sh
@@ -16,7 +16,7 @@ test -z "${HOPR_GITHUB_REF:-}" && (
 # Check if there are changes in the docs/ directory from docs generated steps before
 if [ -n "$(git diff --name-only origin/docs -- docs/)" ]; then
     git add --all docs/
-    git commit -m "${HOPR_GIT_MSG}"å
+    git commit -m "${HOPR_GIT_MSG}"
 
     # must get the latest version of the branch from origin before pushing
     git pull origin docs --rebase --strategy-option recursive -X ours # NB! when pull rebasing, ours is the incoming change (see https://stackoverflow.com/a/3443225)
