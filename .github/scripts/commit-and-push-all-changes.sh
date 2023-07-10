14b2f79f5ff2c8ed3348f8977a8b84e73bec8a77
Luis Martínez <luis.martinez@hoprnet.org>
2023-07-10 15:45:03 -0500

    Revert rebase strategy

--- .github/scripts/commit-and-push-all-changes.sh
+++ .github/scripts/commit-and-push-all-changes.sh
@@ -15,12 +15,12 @@ test -z "${HOPR_GITHUB_REF:-}" && (
 
 # Check if there are changes in the docs/ directory from docs generated steps before
 if [ -n "$(git diff --name-only origin/docs -- docs/)" ]; then
-    git add --all docs/
+    git add .
     git commit -m "${HOPR_GIT_MSG}"
 
     # must get the latest version of the branch from origin before pushing
-    git pull origin docs --rebase --strategy-option recursive -X ours # NB! when pull rebasing, ours is the incoming change (see https://stackoverflow.com/a/3443225)
-    git push origin HEAD:refs/heads/docs                              # Push changes to the 'docs' branch
+    git pull origin docs --rebase --strategy-option recursive -X theirs # NB! when pull rebasing, ours is the incoming change (see https://stackoverflow.com/a/3443225)
+    git push origin HEAD:refs/heads/docs                                # Push changes to the 'docs' branch
 else
     echo "No changes found in the docs/ directory. Skipping commit and push."
 fi
