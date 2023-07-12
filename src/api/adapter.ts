217ad193cb43f1e0e01e826416ab9c061c875573
Diego Alzate <diego35.da@gmail.com>
2023-07-12 14:41:07 -0500

    remove peer info

--- src/api/adapter.ts
+++ src/api/adapter.ts
@@ -2,7 +2,6 @@ import { AccountAdapter } from './account/adapter';
 import { AliasesAdapter } from './aliases/adapter';
 import { ChannelsAdapter } from './channels/adapter';
 import { NodeAdapter } from './node/adapter';
-import { PeerInfoAdapter } from './peerInfo/adapter';
 import { SettingsAdapter } from './settings/adapter';
 import { TicketsAdapter } from './tickets/adapter';
 import { TokensAdapter } from './tokens/adapter';
@@ -12,7 +11,6 @@ export class ApiAdapter {
   public aliases: AliasesAdapter;
   public channels: ChannelsAdapter;
   public node: NodeAdapter;
-  public peerInfo: PeerInfoAdapter;
   public settings: SettingsAdapter;
   public tickets: TicketsAdapter;
   public tokens: TokensAdapter;
@@ -31,7 +29,6 @@ export class ApiAdapter {
     this.aliases = new AliasesAdapter({ apiEndpoint, apiToken, timeout });
     this.channels = new ChannelsAdapter({ apiEndpoint, apiToken, timeout });
     this.node = new NodeAdapter({ apiEndpoint, apiToken, timeout });
-    this.peerInfo = new PeerInfoAdapter({ apiEndpoint, apiToken, timeout });
     this.settings = new SettingsAdapter({ apiEndpoint, apiToken, timeout });
     this.tickets = new TicketsAdapter({ apiEndpoint, apiToken, timeout });
     this.tokens = new TokensAdapter({ apiEndpoint, apiToken, timeout });
