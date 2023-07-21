d60db4c7b0fdeee7eef9c232c736a34fa8e0c14c
Diego Alzate <diego35.da@gmail.com>
2023-07-21 18:16:18 -0500

    add more tests for fetchWithTimeout

--- src/utils/fetchWithTimeout.spec.ts
+++ src/utils/fetchWithTimeout.spec.ts
@@ -14,7 +14,7 @@ describe('fetchWithTimeout', () => {
   it('should abort the request if it takes longer than the specified timeout', async function () {
     const apiEndpoint = new URL(API_ENDPOINT);
     const options = { method: 'GET' };
-    const ms = 1000;
+    const ms = 200;
 
     httpServer = http.createServer((req, res) => {
       setTimeout(() => {
@@ -34,16 +34,85 @@ describe('fetchWithTimeout', () => {
 
     const apiEndpoint = new URL(API_ENDPOINT);
     const options = { method: 'GET' };
-    const ms = 1000;
+    const ms = 200;
 
     httpServer = http.createServer((req, res) => {
-      res.end(JSON.stringify(mockResponse));
+      setTimeout(() => {
+        res.end(JSON.stringify(mockResponse));
+      }, ms); // delay the response
     });
 
     httpServer.listen(PORT);
 
-    const response = await fetchWithTimeout(apiEndpoint, options, ms);
+    const response = await fetchWithTimeout(apiEndpoint, options, ms * 2);
     const jsonResponse = await response.json();
     expect(jsonResponse).toEqual(mockResponse);
+  });
+
+  it('should be able to send multiple requests at the same time', async function () {
+    const mockResponse = { status: 200, data: 'Mock data' };
+
+    const apiEndpoint = new URL(API_ENDPOINT);
+    const options = { method: 'GET' };
+    const ms = 200;
+    const requests = Array.from({ length: 5 });
+
+    httpServer = http.createServer((req, res) => {
+      setTimeout(() => {
+        res.end(JSON.stringify(mockResponse));
+      }, ms); // delay the response
+    });
+
+    httpServer.listen(PORT);
+
+    const responses = [];
+
+    for (const request of requests) {
+      const temp = await fetchWithTimeout(apiEndpoint, options, ms * 2);
+      responses.push(temp);
+    }
+
+    // get back 5 responses for 5 requests
+    expect(responses.length).toEqual(requests.length);
+  });
+
+  it('should throw error if one requests fails while trying to send multiple requests at the same time', async function () {
+    const mockResponse = { status: 200, data: 'Mock data' };
+
+    const apiEndpoint = new URL(API_ENDPOINT);
+    const options = { method: 'GET' };
+    const ms = 200;
+    const requests = Array.from({ length: 5 }).map((value, index) => index);
+
+    httpServer = http.createServer((req, res) => {
+      setTimeout(() => {
+        res.end(JSON.stringify(mockResponse));
+      }, ms); // delay the response
+    });
+
+    httpServer.listen(PORT);
+
+    const responses = [];
+    const errors = [];
+    for (const request of requests) {
+      try {
+        // make one request fail by decreasing ms
+        const temp = await fetchWithTimeout(
+          apiEndpoint,
+          options,
+          request !== 3 ? ms * 2 : ms / 2
+        );
+        responses.push(temp);
+      } catch (e) {
+        errors.push(e);
+      }
+    }
+
+    // get back 4 responses for 5 requests
+    expect(responses.length).toEqual(requests.length - 1);
+
+    // expect to have failed once
+    expect(errors.length).toEqual(1);
+    expect(errors.at(0)).toEqual(new Error('TIMEOUT'));
   });
 });
