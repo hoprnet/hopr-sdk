76e874300f3d9076f217b381f28a82b9c7c71568
Diego Alzate <diego35.da@gmail.com>
2023-07-21 16:49:34 -0500

    remove nock and use http server for fetch with timeout tests

--- src/sdk.spec.ts
+++ src/sdk.spec.ts
@@ -2,13 +2,7 @@ import nock from 'nock';
 import { HoprSDK } from './sdk';
 
 describe('test sdk class', function () {
-  afterEach(function () {
-    jest.clearAllMocks();
-    nock.abortPendingRequests();
-    nock.cleanAll();
-  });
-
-  it('should abort the request with custom timeout', async function () {
+  it('should be able to overwrite timeout', async function () {
     const apiEndpoint = new URL('https://example.com');
     const apiToken = 'token';
     // request delay
@@ -28,32 +22,6 @@ describe('test sdk class', function () {
         recipient: 'recipient',
         path: [],
         timeout: 500
-      })
-    ).rejects.toThrowError('TIMEOUT');
-  });
-  it('should abort the request if request takes longer than class timeout', async function () {
-    const apiEndpoint = new URL('https://example.com');
-    const apiToken = 'token';
-    // request delay
-    const delay = 20000;
-    // class timeout 500s
-    const sdk = new HoprSDK({
-      apiEndpoint: apiEndpoint.origin,
-      apiToken,
-      timeout: 500
-    });
-
-    nock(apiEndpoint.origin)
-      .post('/api/v2/messages')
-      .delay(delay) // Delay the response to trigger a timeout
-      .reply(200, 'Mock data');
-
-    // delay > timeout so it should throw error
-    await expect(
-      sdk.api.messages.sendMessage({
-        body: 'test',
-        recipient: 'recipient',
-        path: []
       })
     ).rejects.toThrowError('TIMEOUT');
   });
--- src/utils/fetchWithTimeout.spec.ts
+++ src/utils/fetchWithTimeout.spec.ts
@@ -1,37 +1,46 @@
 import { fetchWithTimeout } from './fetchWithTimeout';
-import nock from 'nock';
+import http from 'http';
+
+const PORT = 5555;
+const API_ENDPOINT = new URL(`http://localhost:${PORT}`);
 
 describe('fetchWithTimeout', () => {
-  beforeEach(() => {
-    jest.clearAllMocks();
-    nock.cleanAll();
+  let httpServer: http.Server;
+
+  afterEach((done) => {
+    if (httpServer) httpServer.close(done);
   });
 
-  it('should abort the request if it takes longer than the specified timeout', async () => {
-    const apiEndpoint = new URL('https://example.com');
+  it('should abort the request if it takes longer than the specified timeout', async function () {
+    const apiEndpoint = new URL(API_ENDPOINT);
     const options = { method: 'GET' };
     const ms = 1000;
 
-    nock(apiEndpoint.origin)
-      .get(apiEndpoint.pathname)
-      .delay(ms + 100) // Delay the response to trigger a timeout
-      .reply(200, 'Mock data');
+    httpServer = http.createServer((req, res) => {
+      setTimeout(() => {
+        res.end(JSON.stringify({ key: 'value' }));
+      }, ms); // delay the response
+    });
+
+    httpServer.listen(PORT);
 
     await expect(
       fetchWithTimeout(apiEndpoint, options, ms)
     ).rejects.toThrowError('TIMEOUT');
   });
 
-  it('should return the response when the request completes within the timeout', async () => {
+  it('should return the response when the request completes within the timeout', async function () {
     const mockResponse = { status: 200, data: 'Mock data' };
 
-    const apiEndpoint = new URL('https://example.com');
-    const options = { method: 'POST' };
+    const apiEndpoint = new URL(API_ENDPOINT);
+    const options = { method: 'GET' };
     const ms = 1000;
 
-    nock(apiEndpoint.origin)
-      .post(apiEndpoint.pathname)
-      .reply(200, mockResponse);
+    httpServer = http.createServer((req, res) => {
+      res.end(JSON.stringify(mockResponse));
+    });
+
+    httpServer.listen(PORT);
 
     const response = await fetchWithTimeout(apiEndpoint, options, ms);
     const jsonResponse = await response.json();
