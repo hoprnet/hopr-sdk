6916130be6fb9dbd1764bd8fc6c4c453f9ebaf13
Luis Martínez <luis.martinez@hoprnet.org>
2023-10-10 21:46:18 -0500

    Add remaining hopr-admin staking contracts

--- src/ethereum/stakingV2/index.ts
+++ src/ethereum/stakingV2/index.ts
@@ -3,3 +3,8 @@ export * from './hoprNodeSafeRegistryABI';
 export * from './hoprNetworkRegistryABI';
 export * from './hoprNodeModuleABI';
 export * from './hoprNodeStakeFactoryABI';
+export * from './erc721ABI';
+export * from './hoprMultiSendABI';
+export * from './hoprSafeABI';
+export * from './hoprWrapperABI';
+export * from './hoprBoostNFTABI';
