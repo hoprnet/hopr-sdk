220d4040215f52b67c0f63473568d7c6d7ad41be
Michał Jadach <michal.jadach@hoprnet.org>
2024-02-09 11:55:26 +0100

    add network price

---
+++ src/api/network/getNetworkPrice.spec.ts
@@ -0,0 +1,79 @@
+import nock from 'nock';
+import { APIError } from '../../utils';
+import { getNetworkPrice } from './getNetworkPrice';
+import { GetNetworkPriceResponseType } from '../../types';
+
+const API_ENDPOINT = 'http://localhost:3001';
+const API_TOKEN = 'S3CR3T-T0K3N';
+const BUDDY_PEER_ID = '16Uiu2HAmUsJwbECMroQUC29LQZZWsYpYZx1oaM1H9DBoZHLkYn12';
+
+describe('test pingPeer', function () {
+  beforeEach(function () {
+    nock.cleanAll();
+  });
+  it('handles successful response', async function () {
+    nock(API_ENDPOINT)
+      .post(`/api/v3/network/price`)
+      .reply(200, {
+        price: '1000000'
+      } as GetNetworkPriceResponseType);
+
+    const response = await getNetworkPrice({
+      apiToken: API_TOKEN,
+      apiEndpoint: API_ENDPOINT
+    });
+
+    expect(response.price).toEqual('1000000');
+  });
+  it('throws a custom error when hoprd api response is an 400 error', async function () {
+    nock(API_ENDPOINT).post(`/api/v3/network/price`).reply(400, {
+      status: 'INVALID_PEERID'
+    });
+
+    await expect(
+      getNetworkPrice({
+        apiToken: API_TOKEN,
+        apiEndpoint: API_ENDPOINT
+      })
+    ).rejects.toThrow(APIError);
+  });
+  it('throws a custom error when hoprd api response is an 401 error', async function () {
+    nock(API_ENDPOINT).post(`/api/v3/network/price`).reply(401, {
+      status: 'string',
+      error: 'string'
+    });
+
+    await expect(
+      getNetworkPrice({
+        apiToken: API_TOKEN,
+        apiEndpoint: API_ENDPOINT
+      })
+    ).rejects.toThrow(APIError);
+  });
+  it('throws a custom error when hoprd api response is an 403 error', async function () {
+    nock(API_ENDPOINT).post(`/api/v3/network/price`).reply(403, {
+      status: 'string',
+      error: 'string'
+    });
+
+    await expect(
+      getNetworkPrice({
+        apiToken: API_TOKEN,
+        apiEndpoint: API_ENDPOINT
+      })
+    ).rejects.toThrow(APIError);
+  });
+  it('throws a custom error when hoprd api response is an 422 error', async function () {
+    nock(API_ENDPOINT).post(`/api/v3/network/price`).reply(422, {
+      status: 'UNKNOWN_FAILURE',
+      error: 'Full error message.'
+    });
+
+    await expect(
+      getNetworkPrice({
+        apiToken: API_TOKEN,
+        apiEndpoint: API_ENDPOINT
+      })
+    ).rejects.toThrow(APIError);
+  });
+});
---
+++ src/types/network.ts
@@ -0,0 +1,16 @@
+import { string, z } from 'zod';
+import { BasePayload } from './general';
+
+/**
+ * Get network price
+ */
+
+export const GetNetworkPricePayload = BasePayload;
+
+export type GetNetworkPricePayloadType = z.infer<typeof GetNetworkPricePayload>;
+
+export const GetNetworkPriceResponse = z.object({
+  price: z.string()
+});
+
+export type GetNetworkPriceResponseType = z.infer<typeof GetNetworkPriceResponse>;
