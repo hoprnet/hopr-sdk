be073ef203ba6a26ce8e0fe85d325c76783ac9ac
Michał Jadach <michal.jadach@hoprnet.org>
2024-02-12 14:09:34 +0100

    multiaddr nullable

--- src/types/node.ts
+++ src/types/node.ts
@@ -14,7 +14,7 @@ export type GetPeersPayloadType = z.infer<typeof GetPeersPayload>;
 export const Peer = z.object({
   peerId: z.string(),
   peerAddress: z.string(),
-  multiaddr: z.string().optional(),
+  multiaddr: z.string().nullable(),
   heartbeats: z.object({
     sent: z.number(),
     success: z.number()
