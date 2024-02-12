a887102fcf1912ea178ee1672529a433cf10e77c
Michał Jadach <michal.jadach@hoprnet.org>
2024-02-12 14:06:19 +0100

    multiaddr optional

--- src/types/node.ts
+++ src/types/node.ts
@@ -14,7 +14,7 @@ export type GetPeersPayloadType = z.infer<typeof GetPeersPayload>;
 export const Peer = z.object({
   peerId: z.string(),
   peerAddress: z.string(),
-  multiaddr: z.string() || null,
+  multiaddr: z.string().optional(),
   heartbeats: z.object({
     sent: z.number(),
     success: z.number()
