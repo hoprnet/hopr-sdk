dfb28cba7e245a044d8b626282fad42999edb47d
Michał Jadach <michal.jadach@hoprnet.org>
2024-02-26 14:16:31 +0100

    fund channels

--- src/api/channels/index.ts
+++ src/api/channels/index.ts
@@ -2,5 +2,6 @@ export * from './closeChannel';
 export * from './getChannel';
 export * from './getChannels';
 export * from './getChannelTickets';
+export * from './fundChannel';
 export * from './openChannel';
 export * from './redeemChannelTickets';
