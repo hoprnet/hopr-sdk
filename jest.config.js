d2e833b7dc5ae29d7e31093b885f518757f36597
Ronny Esterluss <ronny.esterluss@hoprnet.org>
2024-03-19 14:06:20 +0100

    replaced jest config with ts jest config

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  reporters: [
-    'default',
-    ['./test-reporter.js', { threshold: 2 * 60 * 1000 }] // Customize the threshold as needed
-  ]
-};
