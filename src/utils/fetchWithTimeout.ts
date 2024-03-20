505fb5b5d6d7fb97592a2867adcd0504c797db19
Ronny Esterluss <ronny.esterluss@hoprnet.org>
2024-03-20 15:02:35 +0100

    make nock work with native fetch

--- src/utils/fetchWithTimeout.ts
+++ src/utils/fetchWithTimeout.ts
@@ -11,32 +11,6 @@ export const fetchWithTimeout = (
       // Request was aborted due to the controller signal
       throw new Error('TIMEOUT');
     }
-    if (err.name === 'TypeError') {
-      // AbortSignal not supported - only needed for nock
-      return fallback(apiEndpoint, options, ms);
-    }
     throw err;
   });
 };
-
-function fallback(
-  apiEndpoint: URL | RequestInfo,
-  options?: RequestInit,
-  ms: number = 30000
-) {
-  const controller = new AbortController();
-  const promise = fetch(apiEndpoint, {
-    ...options,
-    signal: controller.signal
-  }).catch((err) => {
-    if (err instanceof Error && err.name === 'AbortError') {
-      // Request was aborted due to the controller signal
-      throw new Error('TIMEOUT');
-    }
-    throw err;
-  });
-  // abort promise if it has not been completed after ms
-  const timeout = setTimeout(() => controller.abort(), ms);
-
-  return promise.finally(() => clearTimeout(timeout));
-}
