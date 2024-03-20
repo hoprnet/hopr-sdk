bba600d2618bb87672c1073c2cd89c0076a44569
Ronny Esterluss <ronny.esterluss@hoprnet.org>
2024-03-20 10:45:46 +0100

    gracefully handling nock shortcomings

--- src/utils/fetchWithTimeout.ts
+++ src/utils/fetchWithTimeout.ts
@@ -1,6 +1,6 @@
 export const fetchWithTimeout = (
   apiEndpoint: URL | RequestInfo,
-  options: RequestInit | undefined,
+  options?: RequestInit,
   ms: number = 30000
 ) => {
   return fetch(apiEndpoint, {
@@ -11,6 +11,31 @@ export const fetchWithTimeout = (
       // Request was aborted due to the controller signal
       throw new Error('TIMEOUT');
     }
-    throw err;
+    if (err.name === 'TypeError') {
+      // AbortSignal not supported - only needed for nock
+      return fallback(apiEndpoint, options, ms);
+    }
   });
 };
+
+function fallback(
+  apiEndpoint: URL | RequestInfo,
+  options?: RequestInit,
+  ms: number = 30000
+) {
+  const controller = new AbortController();
+  const promise = fetch(apiEndpoint, {
+    ...options,
+    signal: controller.signal
+  }).catch((error) => {
+    if (error instanceof Error && error.name === 'AbortError') {
+      // Request was aborted due to the controller signal
+      throw new Error('TIMEOUT');
+    }
+    throw error;
+  });
+  // abort promise if it has not been completed after ms
+  const timeout = setTimeout(() => controller.abort(), ms);
+
+  return promise.finally(() => clearTimeout(timeout));
+}
