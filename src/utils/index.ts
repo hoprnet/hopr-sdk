fd1e638aac7ffbd25625a2a680ab8e34c297ae66
Ronny Esterluss <ronny.esterluss@hoprnet.org>
2024-03-20 15:21:11 +0100

    remove faulty websocket helper and consolidate utils

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,6 +2,3 @@ export * from './log';
 export * from './error';
 export * from './headers';
 export * from './fetchWithTimeout';
-export * from './functions';
-export * from './deferredPromise';
-export { default as WebsocketHelper } from './ws-helper';
