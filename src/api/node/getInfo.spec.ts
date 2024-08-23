17e6d32af841f9640862590bbf9dc151088ed2e2
mjadach-iv <104129084+mjadach-iv@users.noreply.github.com>
2024-08-23 14:09:27 +0200

    added indexer info to getInfo (#127)
    * added indexer info to getInfo
    
    * format

--- src/api/node/getInfo.spec.ts
+++ src/api/node/getInfo.spec.ts
@@ -24,6 +24,10 @@ describe('test getInfo', function () {
         hoprNodeSafeRegistry: '0x3E7c4720934ff6A9FE122Cb761f36a11E9b848D9',
         hoprManagementModule: '0x39b0445b32f5a544eb7917912f5f837bd061be4c',
         hoprNodeSafe: '0x0361a040acb376dd7e5a4643e5a4c7ae9d20c834',
+        indexerBlock: 35346732,
+        indexerChecksum:
+          '0xe780bd95f350e96fe30e98d17560ade3c892f0d3bd75d681f99b0d3c1690517d',
+        indexBlockPrevChecksum: 35611556,
         isEligible: false,
         connectivityStatus: 'Orange',
         channelClosurePeriod: 5
